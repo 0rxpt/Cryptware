86406de189233c3896d4907d47a08b4fd3bb0535
encrypted <91497289+encrxpt3d@users.noreply.github.com>
2023-05-22 22:20:02 -0700

    Update CryptLoad.lua

--- ServerTest/CryptLoad.lua
+++ ServerTest/CryptLoad.lua
@@ -1,13 +1,5 @@
-local CryptServer = require("src/Crypt/CServer")
-local Data = CryptServer:Register({ Name = "Data" }, { "Profile" })
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Crypt = require(ReplicatedStorage.Cryptware.Crypt)
 
-function Data.Get.Profile(player)
-    return "Got: " .. player
-end
-
-function Data.Set.Profile(player, value)
-    return "Set: " .. player .. "\n  to: " .. value
-end
-
-local returned = CryptServer:Get("Data", "Profile", "encrxpt3d")
-print(returned)
+Crypt.RegisterPath(script.Parent.Systems)
+Crypt.Start()
