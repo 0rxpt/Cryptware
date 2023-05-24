7613e236be90a602e561c62ecba41378538e0f56
encrypted <91497289+encrxpt3d@users.noreply.github.com>
2023-05-23 21:10:21 -0700

    Update CServer.lua

--- src/Crypt/CServer.lua
+++ src/Crypt/CServer.lua
@@ -8,8 +8,9 @@ type SystemDef = {
 	[any]: any
 }
 
-type CommDef = {
-	[any]: string
+type ExposeDef = {
+	RE: { [any]: string } | any,
+	RF: { [any]: string } | any,
 }
 
 type System = {
@@ -17,138 +18,193 @@ type System = {
 	[any]: any
 }
 
-type Comm = {
-	[any]: any
-}
+type InvokeType = "Import" | "Systems"
+local InvalidExposeName = { "_Comm", "Name" }
 
-type InvokeType = "Get" | "Set" | "Import" | "Systems"
+local function validateExposeName(exposeName)
+	if exposeName[InvalidExposeName] then
+		return false
+	end
+	return true
+end
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local started = false
 local ready = false
 
+local function createMiddleware()
+	local mdw = Instance.new("RemoteFunction")
+	mdw.Name = "CMiddleware"
+	mdw.Parent = script.Parent
+end
+
 local function createSystemsFolder()
-	local systemsFolder = Instance.new("Folder")
-	systemsFolder.Name = "Systems"
-	systemsFolder.Parent = script.Parent
-	return systemsFolder
-end
-
-local function getSystemsFolder()
-	if not script.Parent:FindFirstChild("Systems") then
-		return createSystemsFolder()
-	else
-		return script.Parent.Systems
+	if script.Parent:FindFirstChild("Systems") then
+		return
 	end
-end
 
-local function createSystemFolder(systemsFolder, systemName)
 	local systemFolder = Instance.new("Folder")
-	systemFolder.Name = systemName
-	systemFolder.Parent = systemsFolder
-	return systemFolder
+	systemFolder.Name = "Systems"
+	systemFolder.Parent = script.Parent
 end
 
-local function getSystemFolder(systemsFolder, systemName)
-	if not systemsFolder:FindFirstChild(systemName) then
-		return createSystemFolder(systemsFolder, systemName)
-	else
+local function createSystemFolder(systemName)
+	local systemsFolder = script.Parent.Systems
+
+	if systemsFolder:FindFirstChild(systemName) then
 		return systemsFolder[systemName]
+	else
+		local systemFolder = Instance.new("Folder")
+		systemFolder.Name = systemName
+		systemFolder.Parent = systemsFolder
+		return systemFolder
 	end
 end
 
-local function createSignal(systemFolder, commName)
-	local rf = Instance.new("RemoteFunction")
-	rf.Name = commName
-	rf.Parent = systemFolder
-	return rf
-end
-
-local function initSignalPath(systemName: string, commName)
-	local systemsFolder = getSystemsFolder()
-	local systemFolder = getSystemFolder(systemsFolder, systemName)
-	
-	assert(not systemFolder:FindFirstChild(commName), "Cannot have duplicate comm names")
-	return createSignal(systemFolder, commName)
-end
-
-local invokeTypes = {
-	["Get"] = function(systemName: string, returnType: string, ...)
-		local system = systems[systemName]
-
-		if system then
-			local get = system.Get[returnType]
-			if get then
-				return get(...)
-			end
-		end
-	end,
-	
-	["Set"] = function(systemName: string, returnType: string, ...)
-		local system = systems[systemName]
-
-		if system then
-			local set = system.Set[returnType]
-			if set then
-				return set(...)
-			end
-		end
+local function createSignal(clientSystem, commName, instanceType)
+	local signal = Instance.new(instanceType)
+	signal.Name = commName
+	signal.Parent = createSystemFolder(clientSystem.Name)
+	if instanceType == "RemoteEvent" then
+		clientSystem._Comm.RE[commName] = signal
+	elseif instanceType == "RemoteFunction" then
+		clientSystem._Comm.RF[commName] = signal
 	end
-}
+	return signal
+end
+
+local function initSignal(clientSystem, commName, instanceType)
+	assert(not clientSystem._Comm[commName], "Cannot have duplicate comm names")
+	return createSignal(clientSystem, commName, instanceType)
+end
 
 local function initSignals()
-	script.Parent.CMiddleware.OnServerInvoke = function(player, invokeType: InvokeType, ...)
+	script.Parent.CMiddleware.OnServerInvoke = function()
 		if not ready then
 			repeat task.wait()
 			until ready
 		end
-		
-		if invokeType == "Import" then
-			return systems[...].Client
-		elseif invokeType == "Systems" then
-			return clientSystems
-		end
-	end
 
+		return clientSystems
+	end
+end
+
+local function findData()
 	for _, system: System in systems do
-		if system.Comm then
-			for commName, signal: RemoteFunction in system.Comm do
-				signal.OnServerInvoke = function(player, invokeType: InvokeType, returnType: string, ...)
-					if invokeTypes[invokeType]
-						and system[invokeType] 
-						and system[invokeType][returnType]
-					then
-						return invokeTypes[invokeType](system.Name, returnType, player, ...)
-					end
-				end
-			end
+		if system.Name:match("Data") then
+			return system
 		end
 	end
 end
 
-function CryptServer.RegisterPath(path: Folder)
+local function initData()
+	local ds = findData()
+
+	if not ds then
+		return
+	end
+
+	if ds.Init then
+		ds:Init()
+	end
+
+	if ds.PlayerAdded then
+		game:GetService("Players").PlayerAdded:Connect(function(player)
+			ds:PlayerAdded(player)
+			ds.Ready = if not ds.Ready then true else nil
+		end)
+	end
+
+	if not ds.Ready then
+		repeat task.wait() until ds.Ready
+	end
+
+	if ds.Start then
+		task.spawn(ds.Start, ds)
+	end
+
+	return ds
+end
+
+function CryptServer.Include(path: Folder)
 	for _, module in path:GetChildren() do
 		require(module)
 	end
 end
 
-function CryptServer.Register(systemDef: SystemDef, commDef: CommDef | nil): System
+function CryptServer.Utils(path: Folder)
+	local utils = {}
+	for _, module in path:GetChildren() do
+		utils[module.Name] = require(module)
+	end
+	for _, system in systems do
+		if not system.Util then
+			system.Util = utils
+		else
+			for utilName, util in utils do
+				system.Util[utilName] = util
+			end
+		end
+	end
+end
+
+function CryptServer.Register(systemDef: SystemDef): System
+	createSystemsFolder()
+
 	local system = systemDef
 
-	if commDef then
-		system.Get = {}
-		system.Set = {}
-		system.Comm = {}
-		system.Client = { Name = system.Name }
-		for _, commName: string in commDef do
-			system.Client[commName] = {}
-			system.Comm[commName] = initSignalPath(system.Name, commName)
-		end
-		clientSystems[system.Name] = {
+	function system.Expose(exposeDef: ExposeDef)
+		assert(not clientSystems[system.Name], "Cannot expose the system more than once")
+
+		local clientSystem = {
 			Name = system.Name,
-			Comm = system.Comm,
-			Client = system.Client
+			_Comm = {}
 		}
+
+		for exposeType: string, exposeData: { [any]: string } in exposeDef do
+			for _, exposeName in exposeData do
+				assert(validateExposeName(exposeName), "Invalid expose name: Cannot use names similar to core methods")
+				assert(not clientSystem[exposeName], "Cannot duplicate comm name " .. exposeName)
+
+				if exposeType == "RE" then
+					clientSystem._Comm.RE = clientSystem._Comm.RE or {}
+					clientSystem[exposeName] = {}
+
+					local re = clientSystem._Comm.RE
+					re[exposeName] = initSignal(clientSystem, exposeName, "RemoteEvent")
+
+					local signal: RemoteEvent = re[exposeName]
+					system[exposeName] = {}
+
+					system[exposeName].Connect = function(_, callback)
+						signal.OnServerEvent:Connect(callback)
+					end
+					system[exposeName].Fire = function(_, player: Player, ...)
+						signal:FireClient(player, ...)
+					end
+					system[exposeName].FireAll = function(_, ...)
+						signal:FireAllClients(...)
+					end
+
+				elseif exposeType == "RF" then
+					clientSystem._Comm.RF = clientSystem._Comm.RF or {}
+					clientSystem[exposeName] = {}
+
+					local rf = clientSystem._Comm.RF
+					rf[exposeName] = initSignal(clientSystem, exposeName, "RemoteFunction")
+
+					local signal: RemoteFunction = rf[exposeName]
+
+					signal.OnServerInvoke = function(...)
+						return system[exposeName](system, ...)
+					end
+				end
+			end
+		end
+
+		clientSystems[clientSystem.Name] = clientSystem
+		system.Expose = nil
+		return system
 	end
 
 	systems[system.Name] = system
@@ -156,39 +212,32 @@ function CryptServer.Register(systemDef: SystemDef, commDef: CommDef | nil): Sys
 end
 
 function CryptServer.Import(system: string)
-	return systems[system].Client
+	return systems[system]
 end
 
 function CryptServer.Start()
-	if started then return end
+	assert(not started, "Cannot start Crypt: Already started!")
 	started = true
-	
+
+	createMiddleware()
 	initSignals()
-	
-	local ds = systems["Data"] or systems["DataSystem"]
-	if ds.Init then
-		ds:Init()
-	end
-	if ds.Start then
-		ds:Start()
-	end
-	
+
+	local ds = initData()
+
 	for _, system in systems do
-		if system.Name == ds.Name then continue end
-		
+		if ds and system.Name == ds.Name then continue end
 		if system.Init then
 			system:Init()
 		end
 	end
-	
-	for _, system in systems do
-		if system.Name == ds.Name then continue end
 
+	for _, system in systems do
+		if ds and system.Name == ds.Name then continue end
 		if system.Start then
 			task.spawn(system.Start, system)
 		end
 	end
-	
+
 	ready = true
 end
 
