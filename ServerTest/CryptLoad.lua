2e47570b1cf4e94d1b6229fd2fdfe33b76b93ce0
encrypted <91497289+encrxpt3d@users.noreply.github.com>
2023-05-23 21:15:46 -0700

    Update CryptLoad.lua

--- ServerTest/CryptLoad.lua
+++ ServerTest/CryptLoad.lua
@@ -3,4 +3,5 @@ local Crypt = require(ReplicatedStorage.Cryptware.Crypt)
 
 Crypt.Include(script.Parent.Systems)
 Crypt.Utils(script.Parent.Utils)
+Crypt.Utils(ReplicatedStorage.Shared)
 Crypt.Start()
