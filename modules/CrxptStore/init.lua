a7d19c06e6103dda4cafd0c1048a4a1c396fd4aa
encrypted <91497289+encrxpt3d@users.noreply.github.com>
2023-08-22 15:18:50 -0700

    Update init.lua

--- modules/CrxptStore/init.lua
+++ modules/CrxptStore/init.lua
@@ -1,31 +1,111 @@
--- Table Definitions
-local CrxptStore = {}
-local CrxptDataStore = {}
-local Account = {}
-
-CrxptDataStore.__index = CrxptDataStore
-
--- Private Variables
 local DataStoreService = game:GetService("DataStoreService")
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+-- Class Definitions
+local CryptStore = {
+    StoreLocked = false,
+    NoSave = false,
+    
+    AutoSaveList = {}    
+}
+
+local CryptDataStore = {}
+CryptDataStore.__index = CryptDataStore
+
+local Account = {}
+
+-- Private Variables
+local placeId = game.PlaceId
+local jobId = game.JobId
+
+local liveCheck = false
+local lastSave = os.clock()
+
+local saveIndex = 1
+local loadIndex = 0
+
+local activeSaveJobs = 0
+local activeLoadJobs = 0
+
+local isStudio = RunService:IsStudio()
+local isLiveCheckActive =false
 
 local Configuration = {
     AutoSaveDuration = 30,
     AssumeDeadSessionLock = 60 * 30
 }
 
--- Class: CrxptDataStore
-function CrxptDataStore.GetStore(storeKey, defaultData)
-    local self = setmetatable({}, CrxptDataStore)
+-- Private Functions
+local function deepCopy(tbl)
+    local newTbl = {}
 
-    self.Store = DataStoreService:GetDataStore(storeKey)
+    for key, value in tbl do
+        newTbl[key] = (type(value) == "table" and deepCopy(value)) or value
+    end
+
+    return newTbl
+end
+
+local function reconcile(tbl, tbl2)
+    for key, value in tbl2 do
+        if type(value) == "table" then
+            tbl[key] = tbl[key] or {}
+			reconcile(tbl[key], value)
+        elseif tbl[key] == nil then
+            tbl[key] = value
+        end
+    end
+end
+
+local function delayUntilLiveAccess()
+    while isLiveCheckActive do
+        task.wait()
+    end
+end
+
+local function saveAccount(account)
+    
+end
+
+-- Class: CryptStore
+function CryptStore.GetStore(storeKey, defaultData)
+    local self = setmetatable({}, CryptDataStore)
+
+    self.GlobalDataStore = nil
     self.StoreKey = storeKey
     self.DefaultData = defaultData
+    self.LoadedAccounts = {}
+    self.LoadJobs = {}
+    self.IsPending = false
+
+    if isLiveCheckActive then
+        self.IsPending = true
+
+        task.spawn(function()
+            delayUntilLiveAccess()
+            
+            if not CryptStore.NoSave then
+                self.GlobalDataStore = DataStoreService:GetDataStore(storeKey)
+            end
+
+            self.IsPending = false
+        end)
+    elseif not CryptStore.NoSave then
+        self.GlobalDataStore = DataStoreService:GetDataStore(storeKey)
+    end
 
     return self
 end
 
-function CrxptDataStore:LoadAccount(playerKey, loadMethod)
+function CryptStore:IsLive()
+    delayUntilLiveAccess()
+
+    return not self.NoSave
+end
+
+-- Class: CryptDataStore
+function CryptDataStore:LoadAccount(playerKey, loadMethod)
     local account
 
     if loadMethod == "Force" then
@@ -33,13 +113,15 @@ function CrxptDataStore:LoadAccount(playerKey, loadMethod)
             return DataStoreService:GetAsync(playerKey)
         end)
 
-        -- error handle
+        if not data then
+
+        end
     end
 
     return account
 end
 
--- Account Class
+-- Class: Account
 function Account:Register(playerKey, loadMethod)
     local self = setmetatable({}, Account)
 
@@ -63,27 +145,94 @@ function Account:OnFree(callback)
     -- Execute callback when account data is freed
 end
 
--- Class: CrxptStore
-function CrxptStore:GetStore(storeKey, defaultData)
-    local store = CrxptDataStore:GetStore(storeKey, defaultData)
-
-    return store
-end
-
 -- Runtime
-task.spawn(function()
-    while task.wait(Configuration.AutoSaveDuration) do
-        
+RunService.Heartbeat:Connect(function()
+    local saveLength = #CryptStore.AutoSaveList
+
+    if saveLength < 1 then
+        return
+    end
+
+    local saveSpeed = Configuration.AutoSaveDuration / saveLength
+    local lastClock = os.clock()
+
+    while lastClock - lastSave > saveSpeed then
+        lastSave += saveSpeed
+
+        local account = CryptStore.AutoSaveList[saveIndex]
+
+        if lastClock - account.LoadTimestamp < Configuration.AutoSaveDuration then
+            account = nil
+
+            for _ = 1, saveLength do
+                saveIndex += 1
+
+                if saveIndex > saveLength then
+                    saveIndex = 1
+                end
+
+                account = CryptStore.AutoSaveList[saveIndex]
+
+                if lastClock - account.LoadTimestamp < Configuration.AutoSaveDuration then
+                    account = nil
+                else
+                    break
+                end
+            end
+        end
+
+        saveIndex += 1
+
+        if saveIndex > saveLength then
+            saveIndex = 1
+        end
+
+        if account then
+            task.spawn(saveAccount, account)
+        end
     end
 end)
 
+-- Initialization
+if isStudio then
+    isLiveCheckActive = true
+
+    task.spawn(function()
+        local status, message = pcall(function()
+            -- This will error if current instance has no Studio API access:
+        	DataStoreService:GetDataStore("____CS"):SetAsync("____CS", os.time())
+        end)
+        
+        local noInternetAccess = not status and message:find("ConnectFail", 1, true) ~= nil
+        
+        if noInternetAccess then
+            warn("[CryptStore]: No internet access - check your network connection")
+        end
+
+        local condition =
+            message:find("403", 1, true) ~= nil -- Cannot write to DataStore from studio if API access is not enabled
+            or message:find("must publish", 1, true) ~= nil -- Game must be published to access live keys
+            or noInternetAccess -- No internet access
+        
+        if not status and condition then
+            CryptStore.NoSave = true
+            print("[CryptStore]: Roblox API services unavailable - data will not be saved")
+        else
+            print("[CryptStore]: Roblox API services available - data will be saved")
+        end
+
+        isLiveCheckActive = false
+    end)
+end
+
 game:BindToClose(function()
-    CrxptStore.Locked = true
+    delayUntilLiveAccess()
+    CryptStore.StoreLocked = true
 
     local activeAccounts = {}
     local jobCount = 0
 
-    for key, account in CrxptStore.AutoSaveList do
+    for key, account in CryptStore.AutoSaveList do
         activeAccounts[key] = account
     end
 
@@ -99,5 +248,4 @@ game:BindToClose(function()
     end
 end)
 
--- Return the CrxptStore class
-return CrxptStore
+return CryptStore
