0f9a96d35afda975ca3b97586564109793c2539e
encrypted <91497289+encrxpt3d@users.noreply.github.com>
2023-08-24 21:43:08 -0700

    Update Usage.lua

--- modules/CryptData/Usage.lua
+++ modules/CryptData/Usage.lua
@@ -1,54 +1,65 @@
 local Players = game:GetService("Players")
 
-local CrxptStore = require(...)
+local CryptData = require(game:GetService("ReplicatedStorage").CryptData)
 local Accounts = {}
 
 local DefaultData = {
-	Cash = 0
+    Cash = 0
 }
 
-local AccountStore = CrxptStore:GetStore("PlayerData", DefaultData)
+local keyVersion = 10
+local AccountStore = CryptData.GetStore("PlayerData_00" .. keyVersion, DefaultData)
 
 local function getAccount(player)
-	return Accounts[player]
+    return Accounts[player]
 end
 
 local function playerAdded(player)
-	local account = AccountStore:LoadAccount("Player_" .. player.UserId)
+    local account = AccountStore:LoadAccount("Player_" .. player.UserId)
 
-	if not account then
-		player:Kick()
-		
-		return
-	end
+    if not account then
+        player:Kick("Please rejoin.")
 
-	account:Reconcile()
-	account:OnFree(function()
-		Accounts[player] = nil
-		player:Kick()
-	end)
+        return
+    end
 
-	if player:IsDescendantOf(Players) then
-		Accounts[player] = account
-		
-		print(account)
-	else
-		account:Free()
-	end
+    account:Reconcile()
+    account:OnFree(function()
+        print("Freed ", player)
+        Accounts[player] = nil
+        player:Kick()
+    end)
+
+    if player:IsDescendantOf(Players) then
+        Accounts[player] = account
+        print("Loaded:", account)
+        
+        coroutine.wrap(function()
+            while task.wait(10) do
+                if not account:IsActive() then
+                    break
+                end
+                
+                account.Data.Cash += 1
+            end
+        end)()
+    else
+        account:Free()
+    end
 end
 
 local function playerRemoving(player)
-	local account = getAccount(player)
+    local account = getAccount(player)
 
-	if not account then
-		return
-	end
+    if not account then
+        return
+    end
 
-	account:Free()
+    account:Free()
 end
 
 for _, player in Players:GetPlayers() do
-	task.spawn(playerAdded, player)
+    task.spawn(playerAdded, player)
 end
 
 Players.PlayerAdded:Connect(playerAdded)
